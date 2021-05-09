08c3f94bbd4d84555665559278f513b7287593ae
rahulrajput <rahulrajput707000@gmail.com>
2021-05-09 21:18:25 +0530

    flatMapWithLombok

---
+++ Interview/src/com/rahul/practice/java8/basicFunctionalities/FlatMapTest.java
@@ -0,0 +1,51 @@
+package com.rahul.practice.java8.basicFunctionalities;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+class Reader {
+	private int id;
+	private String name;
+	private List<String> books;
+
+}
+
+public class FlatMapTest {
+
+	public static void main(String args[]) {
+
+		String[] rahulBooks = { "Maths", "Science", "Java" };
+		String[] gunjanBooks = { "Maths", "Science", "Java" };
+		String[] amarBooks = { "Maths", "Science", "Language" };
+
+		Reader rahul = new Reader(1, "Rahul", Arrays.asList(rahulBooks));
+		Reader gunjan = new Reader(2, "Gunjan", Arrays.asList(gunjanBooks));
+		Reader amar = new Reader(3, "Amar", Arrays.asList(amarBooks));
+
+		List<Reader> readers = new ArrayList<>();
+		readers.add(rahul);
+		readers.add(gunjan);
+		readers.add(amar);
+
+		Integer javaReader = (int) readers.stream().flatMap(reader -> reader.getBooks().stream())
+				.filter(book -> book.equalsIgnoreCase("java")).count();
+		System.out.println("Java Book Readers are:" + javaReader);
+
+	}
+
+}
